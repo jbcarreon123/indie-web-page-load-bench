966329080df560999eed1cdc4262f9e44493ba5b
jbcarreon123 <jbcarreon212@gmail.com>
2025-06-24 16:23:21 +0800

    add page headers

--- start.mjs
+++ start.mjs
@@ -36,6 +36,10 @@ async function getPageMetrics(url, name) {
         }
     })
     let page = await context.newPage();
+    await page.setExtraHTTPHeaders({
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36',
+        'Accept-Language': 'en-US,en;q=0.9'
+    })
     page.removeAllListeners('response');
     page.on('response', async response => {
         try {
