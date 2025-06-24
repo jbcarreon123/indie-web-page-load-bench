7691da04337c5a38dd532300ccb2c43932820c83
jbcarreon123 <jbcarreon212@gmail.com>
2025-06-24 13:07:07 +0800

    dfsbsidfb

---
+++ start.mjs
@@ -0,0 +1,168 @@
+import { chromium } from "playwright";
+import fs from "fs";
+import path from "path";
+
+const PG_TIMEOUT = 60000;
+
+let browser = await chromium.launch({
+    headless: true
+})
+
+let outPath = `./results/`;
+if (!fs.existsSync(outPath)) {
+    fs.mkdirSync(outPath, { recursive: true })
+} else {
+    fs.readdirSync(outPath).forEach((f) => {
+        fs.rmSync(path.join(outPath, f), { recursive: true, force: true })
+    })
+}
+
+const outputFilename = outPath + `metrics-${new Date().toISOString().slice(0, 10)}.json`;
+
+async function getPageMetrics(url, name) {
+    let totalTransferredBytes = 0;
+    let totalSameOriginTransferredBytes = 0;
+    let totalCrossOriginTransferredBytes = 0;
+    let totalTransferredResources = 0;
+    console.log(`[PageMtr] Getting page metrics of URL ${url}... (${name})`);
+    let pagePath = `./results/har/${new URL(url).hostname}`;
+    if (!fs.existsSync(pagePath)) {
+        fs.mkdirSync(pagePath, { recursive: true })
+    }
+    let context = await browser.newContext({
+        recordHar: {
+            content: 'omit',
+            path: pagePath + `/results-${name}.har`
+        }
+    })
+    let page = await context.newPage();
+    page.removeAllListeners('response');
+    page.on('response', async response => {
+        try {
+            if (response.status() < 200 || response.status() >= 400) {
+                return;
+            }
+            const headers = response.headers();
+            const contentLength = headers['content-length'];
+            if (contentLength) {
+                totalTransferredBytes += parseInt(contentLength, 10);
+                if (new URL(response.url()).hostname == new URL(url).hostname) {
+                    totalSameOriginTransferredBytes += parseInt(contentLength, 10)
+                } else {
+                    totalCrossOriginTransferredBytes += parseInt(contentLength, 10)
+                }
+            } else {
+                const buffer = await response.body();
+                totalTransferredBytes += buffer.length;
+                if (new URL(response.url()).hostname == new URL(url).hostname) {
+                    totalSameOriginTransferredBytes += parseInt(buffer.length, 10)
+                } else {
+                    totalCrossOriginTransferredBytes += parseInt(buffer.length, 10)
+                }
+            }
+            totalTransferredResources++;
+        } catch (error) {
+            console.warn(`[NetMoni] Error processing response: ${error.message}`);
+        }
+    });
+    try {
+        await page.goto(url, { waitUntil: 'commit' });
+        if (new URL(page.url()).hostname != new URL(url).hostname && new URL(page.url()).pathname != new URL(url).pathname) {
+            console.log(`[PageMtr] Redirects found, redirecting to ${page.url()}`);
+            totalCrossOriginTransferredBytes = 0;
+            totalSameOriginTransferredBytes = 0;
+            totalTransferredBytes = 0;
+            totalTransferredResources = 0;
+        }
+        await page.waitForLoadState('load', { timeout: PG_TIMEOUT });
+        const performanceTiming = await page.evaluate(() => {
+            return performance.getEntriesByType('navigation')[0];
+        });
+        const domElementCount = await page.evaluate(() => {
+            return document.querySelectorAll('*').length;
+        });
+
+        let otherPage = "N/A";
+        const locators = page.locator('a');
+        const count = await locators.count();
+
+        if (count > 0) {
+            for (let i = 0; i < count; i++) {
+                const el = locators.nth(i);
+                let href = await el.getAttribute('href');
+                try {
+                    let u = new URL(href);
+                    if (u.hostname != new URL(page.url()).hostname) continue;
+                } catch {}
+                if (href !== "/" && href !== "/index.html" && href) {
+                    otherPage = href;
+                    break;
+                }
+            }
+        }
+
+        await page.close();
+        await context.close();
+
+        return {
+            pageLoadTime_ms: parseFloat((performanceTiming.loadEventEnd - performanceTiming.startTime).toFixed(2)),
+            domContentLoadedTime_ms: parseFloat((performanceTiming.domContentLoadedEventEnd - performanceTiming.startTime).toFixed(2)),
+            totalTransferred_bytes: totalTransferredBytes,
+            totalSameOriginTransferred_bytes: totalSameOriginTransferredBytes,
+            totalCrossOriginTransferred_bytes: totalCrossOriginTransferredBytes,
+            totalTransferredResources: totalTransferredResources,
+            domElementCount: domElementCount,
+            actualPage: page.url(),
+            otherPage: otherPage
+        }
+    } catch (e) {
+        console.error(e);
+        return {
+            pageLoadTime_ms: 'N/A',
+            domContentLoadedTime_ms: 'N/A',
+            totalTransferred_bytes: 'N/A',
+            totalSameOriginTransferred_bytes: 'N/A',
+            totalCrossOriginTransferred_bytes: 'N/A',
+            totalTransferredResources: 'N/A',
+            domElementCount: 'N/A',
+            actualPage: 'N/A',
+            otherPage: 'N/A'
+        }
+    }
+}
+
+async function loadPages() {
+    let jsonFile = fs.readFileSync('./pages.json', 'utf-8');
+    /**  @type {string[]} */
+    let json = JSON.parse(jsonFile);
+
+    let pageDatas = [];
+
+    for (let i = 0; i < json.length; i++) {
+        let site = json[i];
+        let indexPageMetricTimes = [];
+        let otherPageMetricTimes = [];
+        for (let i = 1; i <= 3; i++) {
+            indexPageMetricTimes.push(await getPageMetrics(site, 'iter-' + i));
+        }
+        if (indexPageMetricTimes[2].otherPage != "N/A") {
+            for (let i = 1; i <= 3; i++) {
+                otherPageMetricTimes.push(await getPageMetrics(site + indexPageMetricTimes[2].otherPage, 'other-iter-' + i));
+            }
+        }
+        let pageData = {
+            siteUrl: site,
+            actualSiteUrl: indexPageMetricTimes[0].actualPage,
+            metrics: indexPageMetricTimes,
+            otherMetrics: otherPageMetricTimes
+        }
+        console.log(pageData);
+        pageDatas.push(pageData);
+    }
+
+    fs.writeFileSync(outputFilename, JSON.stringify(pageDatas));
+}
+
+await loadPages();
+
+await browser.close();
